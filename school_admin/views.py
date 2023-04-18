a3a6d298f6234ef93872c08529cea8f00e422563
hisham-slm <hishamsalam350@gmail.com>
2023-04-18 12:50:33 +0530

    teacher added

--- school_admin/views.py
+++ school_admin/views.py
@@ -3,6 +3,7 @@ from .models import *
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TeacherSerializer
  
 
 @api_view(['POST'])
@@ -18,4 +19,26 @@ def login(request):
         return JsonResponse ({'statusCode': 201, 'token': 'school-admin'})
     except :
         
-        return JsonResponse ({'statusCode': 404})  
+        return JsonResponse ({'statusCode': 404})
+
+@api_view(['POST'])
+def add_teacher(request):
+    msg = ''
+    try:
+        params = request.data
+
+        teacher_data = TeacherSerializer(data = params)
+
+        if teacher_data.is_valid():
+            teacher_data.save()
+            msg = "Teacher Added"
+            statusCode = 204
+        else:
+            msg = 'Form Error'
+            statusCode = 403
+    except:
+        msg = 'Something went wrong'
+        statusCode = 505
+    return JsonResponse({'statusCode' : statusCode, 'msg' : msg})
+
+    
